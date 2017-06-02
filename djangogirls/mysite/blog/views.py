7b0b42c851649876125da90c1262a948c09ab672
DESKTOP-7S9O4DU\Fatma <Fatma_Habib94@hotmail.com>
2017-06-02 11:37:51 +0200

    no message

--- djangogirls/mysite/blog/views.py
+++ djangogirls/mysite/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Chat
 
 # Create your views here.
 def chat_list(request):
-    return render(request, 'blog/chat_list.html', {})
+    chats=Chat.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/chat_list.html',  {'chats': chats})
+
+
