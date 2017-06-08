728e660c2e933642f396b1dccea7e47d99e31aeb
DESKTOP-7S9O4DU\Fatma <Fatma_Habib94@hotmail.com>
2017-06-08 21:24:20 +0200

    no message

--- djangogirls/mysite/mysite/wsgi.py
+++ djangogirls/mysite/mysite/wsgi.py
@@ -1,16 +1,33 @@
 """
-WSGI config for mysite project.
+WSGI config for wayup project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from configurations import importer
+
+
+dotenv.read_dotenv(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", ".env")
+    )
+)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
+
+importer.install()
 
 application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
